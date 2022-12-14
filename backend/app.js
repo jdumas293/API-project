66b300afa0243fa31eb716dc97c51124d9b7a46c
Joseph Dumas <jdumas293@gmail.com>
2022-12-14 15:50:15 -0500

    add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -5,18 +5,19 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-
 const routes = require('./routes'); // Add routes to the Express app
+const { ValidationError } = require('sequelize'); // Sequelize error handler
 
 const { environment } = require('./config');
 const isProduction = environment === 'production'; // will be true if the environment is in production
 
-const app = express();
+const app = express(); // Initialize Express
+
 
 app.use(morgan('dev')); // for logging information about requests and responses
-
 app.use(cookieParser()); // for parsing cookies
 app.use(express.json()); // for parsing JSON bodies of requests
+
 
 // Security Middleware
 if (!isProduction) {
@@ -42,6 +43,40 @@ app.use(
     })
 );
 
+
 app.use(routes); // Connect all the routes
+
+
+// Error Handling Middleware
+// Catch unhandled requests and forward to error handler
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err); // error handlers defined after this middleware will be invoked
+});
+
+// Sequelize Error-Handler
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+});
+
+// Error Formatter Error-Handler
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
 
 module.exports = app;
