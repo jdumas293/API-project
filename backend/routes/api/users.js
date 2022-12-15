a0140d0a6346f9b6e23a9b88e7d703bb02204ce5
Joseph Dumas <jdumas293@gmail.com>
2022-12-15 17:44:24 -0500

    add User signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,20 @@
 const express = require('express');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
+
+// Sign up
+router.post('/', async (req, res) => {
+    const { email, password, username } = req.body;
+    const user = await User.signup({ email, username, password }); // call signup static method from User model
+
+    await setTokenCookie(res, user); // if user successfully created - set token cookie
+
+    return res.json({
+        user: user
+    });
+});
 
 module.exports = router;
